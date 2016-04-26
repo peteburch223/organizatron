ddd0887ca4444de66afb8d4d4a02f35f7a42a820
Pete Burch <peteburch223@gmail>
2016-04-26 09:47:51 +0100

    get & post for materials, get for tags. materials error post not working

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -2,4 +2,22 @@ class MaterialsController < ApplicationController
   def index
     render json: Material.all
   end
+
+  def create
+    material = Material.new(material_params)
+    if material.save
+      render json: material, status: :created
+    else
+      respond_with_errors(material)
+    end
+  end
+
+  def material_params
+    params.require(:material).permit(:title, :description, :link_url)
+  end
+
+  def respond_with_errors(object)
+    render json: {errors: ErrorSerializer.serialize(object)}, status: :unprocessable_entity
+  end
+
 end
--- spec/requests/materials_spec.rb
+++ spec/requests/materials_spec.rb
@@ -16,4 +16,48 @@ describe "materials API" do
       expect(body["description"]).to eq expected_result.description
     end
   end
-end
+
+
+  describe "POST /materials" do
+    it 'creates a material' do
+      title = "Yelp V1 walkthrough"
+      link_url = "https://github.com/makersacademy/course/blob/master/rails/yelpv1.md#installing-rails-and-initialising-your-app"
+      description = "would be a bit stuck without this one"
+
+      material_params = {
+        "material" => {
+          "title" => title,
+          "link_url" => link_url,
+          "description" => description
+          }
+        }.to_json
+
+      request_headers = {
+        "Accept" => "application/json",
+        "Content-Type" => "application/json"
+      }
+
+      post "/materials", material_params, request_headers
+      expect(response.status).to eq 201
+      expect(Material.first.title).to eq title
+      expect(Material.first.link_url).to eq link_url
+      expect(Material.first.description).to eq description
+    end
+
+    xit "responds with errors" do
+
+      material_params = {
+        "material" => {}
+        }.to_json
+
+      request_headers = {
+        "Accept" => "application/json",
+        "Content-Type" => "application/json"
+      }
+
+      post "/materials", material_params, request_headers
+
+      expect(response.status).to eq(422)
+    end
+  end
+ end
